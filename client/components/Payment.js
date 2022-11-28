423f5ff93c4940d97206b297d2bf8b85ae0521a2
Ethan <elnair122@gmail.com>
2022-11-28 14:03:07 -0500

    cssCheckoutPageUpdated

--- client/components/Payment.js
+++ client/components/Payment.js
@@ -16,7 +16,7 @@ export class Payment extends React.Component {
 
   render() {
     const { submitPayment } = this;
-    console.log(this.props.auth);
+
     const products = this.props.cart.products || [];
     let cartItems = products.reduce(function (accum, obj) {
       const {
@@ -32,28 +32,77 @@ export class Payment extends React.Component {
     }, 0);
 
     return (
-      <section className="bg-whiteblue container" id='carousel'>
-        <ul>
+      <div>
+        <ul
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "30px",
+            border: "2px solid lightgray",
+            padding: "50px",
+            marginBottom: "50px",
+          }}
+        >
           {products.map((product, index) => {
             return (
-              <div key={index}>
+              <div style={{ display: "flex" }} key={index}>
                 <img src={product.imageSmall} />
-                <h4>Name: {product.name}</h4>
-                <h4>Price: ${product.price / 100}</h4>
-                <h4></h4>
+                <div style={{ marginLeft: "30px" }}>
+                  <h1>{product.name}</h1>
+                  <div style={{ display: "flex", gap: "10px" }}>
+                    <h1 style={{ color: "maroon" }}>
+                      {" "}
+                      ${product.price / 100}{" "}
+                    </h1>
+                    <h1 style={{ color: "black" }}> & </h1>
+                    <h1 style={{ color: "rgb(81, 129, 142)" }}>FREE Returns</h1>
+                  </div>
+                  <div>
+                    <h2>Qty: {product.Order_Product.quantity}</h2>
+                  </div>
+                  <h4></h4>
+                </div>
               </div>
             );
           })}
         </ul>
-        <div>
-          <h2>Total items: {cartItems}</h2>
-          <h2>Subtotal: ${total / 100}</h2>
+        <div
+          style={{
+            border: "2px solid lightgray",
+            height: "150px",
+            display: "flex",
+            gap: "30px",
+            marginBottom: "10vh",
+          }}
+        >
+          <button
+            style={{
+              marginLeft: "50px",
+              height: "70px",
+
+              width: "300px",
+              alignSelf: "center",
+              backgroundColor: "rgb(255, 217, 0)",
+              borderRadius: "20px",
+              border: "0px",
+            }}
+            onClick={() => {
+              submitPayment();
+              alert("Thank you for your purchase!");
+            }}
+          >
+            <h1>Place your order</h1>
+          </button>
+          <div style={{ alignSelf: "center" }}>
+            <h1 style={{ color: "maroon", fontSize: "36px" }}>
+              Order Total: ${total / 100}
+            </h1>
+            <h3>
+              By placing your order, you agree to the terms and the conditions
+            </h3>
+          </div>
         </div>
-        <button onClick={() => {
-          submitPayment();
-          alert("Thank you for your purchase!");
-        }}>Make a payment</button>
-      </section>
+      </div>
     );
   }
 }
